d0a80d521d0c696a5aa79b66a9890a15f26b5128
baylarvaliyev <baylarv@gmail.com>
2026-06-30 00:33:31 +0400

    Add Meta Pixel (id 1025597226725302) site-wide via next/script with afterInteractive strategy in the root layout - the correct Next.js pattern for third-party tracking scripts, loading after the page is interactive rather than blocking initial render. Includes the standard noscript fallback pixel image for users with JS disabled.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Fraunces, Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
+import Script from "next/script";
 import PageTransition from "@/components/PageTransition";
 import "./globals.css";
 
@@ -43,6 +44,31 @@ export default function RootLayout({
       className={`${fraunces.variable} ${inter.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col bg-[#0B1026] text-[#F5F3EE]">
+        {/* Meta Pixel */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1025597226725302');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1025597226725302&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <PageTransition>{children}</PageTransition>
         <Analytics />
       </body>
